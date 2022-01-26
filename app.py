aeaad2b020cb6950897a4d5870375d6360802ce2
Max Conroy <max.conroy@outlook.com>
2022-01-25 19:27:03 -0500

    added requirements and main app file

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+	return  'You are at the home page'
+
+@app.route('/about')
+def about():
+	return 'You are at the about page'
+
+@app.route('/dostuff')
+def dostuff():
+	return 'App does stuff here'
+
+if __name__ == '__main__':
+	app.run()
