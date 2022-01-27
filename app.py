cefa6a341c75a4cb2434cc478b5f5d4fda23ac63
Max Conroy <max.conroy@outlook.com>
2022-01-27 15:52:17 -0500

    updated web app

--- app.py
+++ app.py
@@ -4,15 +4,15 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-	return  'You are at the home page'
+	return 'You are at the home page \n'
 
 @app.route('/about')
 def about():
-	return 'You are at the about page'
+	return 'You are at the about page \n'
 
 @app.route('/dostuff')
 def dostuff():
-	return 'App does stuff here'
+	return 'App does stuff here \n'
 
 if __name__ == '__main__':
 	app.run(debug=True, host='0.0.0.0')
